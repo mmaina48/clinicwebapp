d57aff66405e0cecee12a9e22e3a22c7c7c0f74f
mmaina48 <mmaina48@gmail.com>
2020-07-03 18:50:29 +0300

    addedinner navigation buttons and change delete modal button color

--- routes.py
+++ routes.py
@@ -70,14 +70,12 @@ def AllCustomers():
     customers=Customer.query.all()
     return render_template('allCustomers.html',customers=customers)
 
-#add patient
+#add patient by save 
 @app.route('/addpatient/',methods=['GET','POST'])
 @login_required
 def AddCustomer():
     # generating an patient id function
-    
     pid=db.session.query(db.func.max(Customer.id)).one()
-    print(pid)
     def patientid(setids):
         import datetime
         id=['0' if v is None else v for v in setids]
@@ -86,7 +84,7 @@ def AddCustomer():
         return patient_id
     
     opd=patientid(pid)
-    
+
     if request.method == 'POST':
         newcustomer = Customer(patient_id=request.form['patient_opd'],name=request.form['patient_name'],age=request.form['age'],gender=request.form['gendertype'],patient_phone=request.form['patient_phone'],\
             nhif_no=request.form['patient_nhif_no'],National_id=request.form['patient_National_id'])
@@ -94,14 +92,16 @@ def AddCustomer():
         try:
             db.session.commit()
             flash(f' {newcustomer.name} Successfully Added!', 'success')
-            return redirect(url_for('AllCustomers'))
+            return redirect(url_for('AddCustomer'))
         except IntegrityError:
             db.session.rollback()
             flash(f'This customer already exists','danger')
             return redirect(url_for('AddCustomer'))
     else:
         return render_template('addCustomer.html',opd=opd)
-    
+
+
+
 #Edit Patient
 @app.route('/patients/<int:customer_id>/edit/', methods = ['GET', 'POST'])
 @login_required
@@ -296,6 +296,7 @@ def AllInvoices():
     allinvoices=Order.query.all()
     return render_template('allInvoices.html',allinvoices=allinvoices)
 
+# Delete invoice
 @app.route('/delete/<int:invoice_id>/delete/', methods = ['POST'])
 @login_required
 def deleteInvoice(invoice_id):
@@ -339,6 +340,7 @@ def deleteInvoice(invoice_id):
 
         flash(f'Invoice  successfully Deleted!','danger')
         return redirect(url_for('AllInvoices'))
+
 # show all items in an order
 @app.route('/InvoiceDetails/<int:invoice_id>/',methods=['GET','POST'])
 @login_required
@@ -408,6 +410,7 @@ def deleteProduct(product_id):
         db.session.commit()
         flash(f'product successfully Delete!','danger')
         return redirect(url_for('allProducts', product_id = product_id))
+
 # API ENDPOINT
 @app.route('/productprice/<product>')
 def Productprice(product):
